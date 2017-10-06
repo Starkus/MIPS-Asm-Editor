1402da2b2aecd697fe2fcf88fdc46fc21fc792d7
Starkus <emilianoalejosenega@gmail.com>
2017-10-06 13:58:49 -0300

    Tooltips for hex literals.

--- MIPSASMEditor/src/net/starkus/mipseditor/assistant/Tooltips.java
+++ MIPSASMEditor/src/net/starkus/mipseditor/assistant/Tooltips.java
@@ -19,29 +19,68 @@ public class Tooltips {
 		title.getStyleClass().add("tooltip-title");
 		desc.getStyleClass().add("tooltip-content");
 		
-		if (matcher.find(StringUtils.startOfWord(code, index)) && (matcher.group("OPCODE") != null || 
+		if (matcher.find(StringUtils.startOfWord(code, index)))
+		{
+			String word = code.substring(matcher.start(), matcher.end());
+		
+			if ((matcher.group("OPCODE") != null || 
 						matcher.group("DIRECTIVE") != null ||
 						matcher.group("REGISTERNAME") != null) || 
 						matcher.group("DEFINECALL") != null)
-		{
+			{
+				
+				if (word.startsWith("@"))
+					word = word.substring(1);
+				
+				HashMap<String, Keyword> keywords = Assistant.getKeywordBank().getKeywords(); 
+				Keyword k = keywords.get(word);
+				
+				if (k == null)
+					return false;
+				
+				title.setText(k.getTooltipTitle());
+				desc.setText(k.getTooltipBody());
+				
+				title.setText(title.getText() + "\n");
+				flow.getChildren().setAll(title, desc);
+				
+				return true;
+			}
 			
-			String word = code.substring(matcher.start(), matcher.end());
-			if (word.startsWith("@"))
-				word = word.substring(1);
-			
-			HashMap<String, Keyword> keywords = Assistant.getKeywordBank().getKeywords(); 
-			Keyword k = keywords.get(word);
-			
-			if (k == null)
-				return false;
-			
-			title.setText(k.getTooltipTitle());
-			desc.setText(k.getTooltipBody());
-			
-			title.setText(title.getText() + "\n");
-			flow.getChildren().setAll(title, desc);
-			
-			return true;
+			else if (matcher.group("LITERAL") != null)
+			{
+				Long literal;
+				
+				if (word.toLowerCase().startsWith("0x"))
+				{
+					word = word.substring(2);
+				}
+				else if (word.startsWith("$"))
+				{
+					word = word.substring(1);
+				}
+				
+				literal = Long.parseLong(word, 16);
+
+				Float f = Float.intBitsToFloat(literal.intValue());
+				Float fu = Float.intBitsToFloat(literal.intValue() * 65536);
+				
+				// Signed word
+				Long signedWord = literal;
+				if (signedWord > 32767)
+					signedWord -= 65536;
+				
+				title.setText(word);
+				desc.setText("Decimal value: " + Long.toString(literal)
+							+"\nSigned word decimal: " + Long.toString(signedWord)
+							+"\nFloat value: " + Float.toString(f)
+							+"\nFloat value (if upper): " + Float.toString(fu));
+				
+				title.setText(title.getText() + "\n");
+				flow.getChildren().setAll(title, desc);
+				
+				return true;
+			}
 		}
 		
 		return false;
